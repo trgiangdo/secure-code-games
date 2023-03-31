d83d736fc715beb8b029795c3b23f75f1001cae2
trgiangdo <dtr.giang.1299@gmail.com>
2023-03-31 21:21:58 +0700

    level 1: limit the amount, quantity, and net value

--- Level-1/code.py
+++ Level-1/code.py
@@ -1,7 +1,7 @@
 '''
 ////////////////////////////////////////////////////////////
 ///                                                      ///
-///   0. tests.py is passing but the code is vulnerable  /// 
+///   0. tests.py is passing but the code is vulnerable  ///
 ///   1. Review the code. Can you spot the bug?          ///
 ///   2. Fix the code but ensure that tests.py passes    ///
 ///   3. Run hack.py and if passing then CONGRATS!       ///
@@ -13,20 +13,43 @@
 
 from collections import namedtuple
 
+MAX_AMOUNT = 1000000
+MAX_QUANTITY = 1000
+MAX_NET = 1000000
+
 Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
 
+
 def validorder(order: Order):
     net = 0
-    
+
     for item in order.items:
+
         if item.type == 'payment':
+            if item.amount >= MAX_AMOUNT or item.amount <= - MAX_AMOUNT:
+                print(f"Invalid amount: {item.amount} for item {str(item)}.")
+                continue
+
             net += item.amount
+
         elif item.type == 'product':
+            if item.amount >= MAX_AMOUNT or item.amount <= - MAX_AMOUNT:
+                print(f"Invalid amount: {item.amount} for item {str(item)}.")
+                continue
+
+            if item.quantity >= MAX_QUANTITY or item.quantity < 0:
+                print(f"Invalid quantity: {item.quantity} for item {str(item)}.")
+                continue
+
             net -= item.amount * item.quantity
+
+            if net >= MAX_NET:
+                return(f"The total amount has exceeded the maxinum value: {net}")
+
         else:
             return("Invalid item type: %s" % item.type)
-    
+
     if net != 0:
         return("Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net))
     else:
